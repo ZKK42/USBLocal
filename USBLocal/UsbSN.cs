8af93497146c94e869fc543d538fa4e123998006
Administrator <Administrator@ZKK>
2019-11-02 14:31:01 +0300

    Add project files.

---
+++ USBLocal/UsbSN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Management;
+
+namespace USBLocal
+{
+    class UsbSN
+    {
+        public bool Usbserinokontrol()
+        {
+            bool kontrol = false;
+            string serino = "4C530499930219119355";
+            ManagementObjectSearcher usb = new ManagementObjectSearcher("Select * from Win32_DiskDrive where InterfaceType='USB'");
+            foreach (var usbveri in usb.Get())
+            {
+                if (usbveri["SerialNumber"].ToString() == serino)
+                    kontrol = true;
+                Console.WriteLine("USB SERİ NO :"+usbveri["SerialNumber"].ToString());
+            }
+            return kontrol=true;
+        }
+    }
+}
